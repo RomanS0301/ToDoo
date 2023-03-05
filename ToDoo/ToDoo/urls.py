b8a8d14e34c3c231c53907626911ed33fefbb387
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-03-05 17:41:26 +0300

    Создана форма задач на странице пользователя

--- ToDoo/ToDoo/urls.py
+++ ToDoo/ToDoo/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('logout', views.logoutuser, name='logoutuser'),
     # todos
     path('', views.home, name='home'),
+    path('create', views.createtodo, name='createtodo'),
     path('current/', views.currenttodos, name='currenttodos'),  # страница текущего пользователя
 
 
